4d5aa6bd181bfd48c916794ecb5bbe776bc22023
Urva Chheda <140619305+Urva13@users.noreply.github.com>
2024-07-31 16:19:58 +0530

    Add files via upload

---
+++ Project6.py
@@ -0,0 +1,40 @@
+import os
+def add(a,b):
+    return a+b
+def substract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
+operations_dict = {
+    "+" : add,
+    "-" : substract,
+    "*" : multiply,
+    "/" : divide
+}
+def calculator():
+    number1 = float(input("Enter first number:"))
+    for symbol in operations_dict:
+        print(symbol)
+    
+    continue_flag=True
+    while continue_flag:
+        op_symbol = input("Pick an operation:")
+        number2 = float(input("Enter Next number:"))
+        calulator_function = operations_dict[op_symbol]
+        output = calulator_function(number1, number2)
+        print(F"{number1} {op_symbol} {number2} = {output}")
+        
+        should_continue = input(f"Enter 'y' to continue calculation with {output} or 'n' to start to new calculation or 'x' to exit :").lower()
+        if should_continue == "y":
+            number1 = output
+        elif should_continue == "n":
+            continue_flag = False
+            os.system('cls')
+            calculator()
+        else:
+            continue_flag = False
+            print("Bye")
+calculator()
